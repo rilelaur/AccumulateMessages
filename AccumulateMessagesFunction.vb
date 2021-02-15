b55d93d5d740806dae540c38e815d7c6f813ef0f
Laura <laurakriley01@gmail.com>
2021-02-14 19:57:27 -0700

    add comments

--- AccumulateMessagesFunction.vb
+++ AccumulateMessagesFunction.vb
@@ -2,12 +2,13 @@
 'RCET 0265
 'Spring 2021
 'Accumulate Messages Function
-'
+'https://github.com/rilelaur/AccumulateMessages.git
 
 Option Explicit On
 Option Strict On
 Module AccumulateMessagesFunction
 
+    'asks the user to enter a message and concatinates the input by the user
     Sub Main()
         Dim userInput As String
         Console.WriteLine("Please enter a message")
@@ -17,6 +18,7 @@ Module AccumulateMessagesFunction
         Console.Read()
     End Sub
 
+    'tells the user what they entered and returns it as a string
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
         Static messages As String
 
